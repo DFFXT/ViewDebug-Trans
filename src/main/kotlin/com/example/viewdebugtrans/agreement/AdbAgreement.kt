afba0ddb8b43533b3d0b596373236c8fea22e86a
非想非 <1559379562@qq.com>
2023-12-08 23:10:45 +0800

    修复版本判断异常问题

--- src/main/kotlin/com/example/viewdebugtrans/agreement/AdbAgreement.kt
+++ src/main/kotlin/com/example/viewdebugtrans/agreement/AdbAgreement.kt
@@ -1,6 +1,6 @@
 package com.example.viewdebugtrans.agreement
 
-data class AdbAgreement (
+data class AdbAgreement(
     // 格式 0.20.4-1-g87125dd 或者不带后续 0.20.4
     val version: String,
     val pkgName: String,
@@ -14,15 +14,18 @@ data class AdbAgreement (
      * @param version 衡量版本, 格式必须为 xx.xx.xx 不能带更详细的版本号
      * @return true, 客户端版本高于等于指定版本
      */
-    fun versionSupport(version: String): Boolean{
+    fun atLeast(version: String): Boolean {
         val versionArray = version.split('.').map { it.toIntOrNull() ?: 0 }
-        val thisVersion = this.version.replace('-','.').split('.').map { it.toIntOrNull() ?: 0 }
+        val thisVersion = this.version.replace('-', '.').split('.').map { it.toIntOrNull() ?: 0 }
         if (versionArray.size >= 3 && thisVersion.size >= 3) {
-            return !(versionArray[0] > thisVersion[0] || versionArray[1] > thisVersion[1] || versionArray[2] > thisVersion[2])
+            val versionValue = versionArray[0] * 10000_0000 + versionArray[1] * 10000 + versionArray[2]
+            val thisValue = thisVersion[0] * 10000_0000 + thisVersion[1] * 10000 + thisVersion[2]
+            return thisValue >= versionValue
         } else {
             throw RuntimeException("不规范的版本号:${this.version}")
         }
     }
+
     companion object {
         fun parse(map: Map<String, String>): AdbAgreement {
             return AdbAgreement(
